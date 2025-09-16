15a99374de82940926eaf4c32732548971135626
najibean <najibullah89@gmail.com>
2025-09-16 20:27:21 +0700

    update ApiResponse

--- src/main/java/najib/io/utils/apiresponse/ApiResponse.java
+++ src/main/java/najib/io/utils/apiresponse/ApiResponse.java
@@ -18,6 +18,10 @@ public class ApiResponse<T> {
         return new ApiResponse<>(true, "Success", data);
     }
 
+    public static <T> ApiResponse<T> ok(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
     public static <T> ApiResponse<T> fail(String message) {
         return new ApiResponse<>(false, message, null);
     }
