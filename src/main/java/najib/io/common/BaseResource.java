cd38075586fe312c7de66f868aa83235a868ca8c
najib-ean <najibullah89@gmail.com>
2025-12-24 07:28:18 +0700

    fix BaseResource, BaseService

--- src/main/java/najib/io/common/BaseResource.java
+++ src/main/java/najib/io/common/BaseResource.java
@@ -1,12 +1,17 @@
 package najib.io.common;
 
+import jakarta.validation.Valid;
+import jakarta.validation.groups.ConvertGroup;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import najib.io.utils.apiResponse.ApiResponse;
 import najib.io.utils.apiResponse.PaginatedResponse;
+import najib.io.utils.validation.OnCreate;
+import najib.io.utils.validation.OnUpdate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Produces(MediaType.APPLICATION_JSON)
@@ -48,32 +53,32 @@ public abstract class BaseResource<Entity extends BaseEntity, ReqDto, ResDto ext
 
         String message = entities.isEmpty() ? "No data found" : "Success get all " + moduleName() + "s";
 
-        return PaginatedResponse.success(message, mapper().toResponse(entities), paginationDto);
+        return PaginatedResponse.success(message, service().toDto(entities), paginationDto);
     }
 
     @GET
     @Path("/{id}")
-    public ApiResponse<ResDto> getOne(@PathParam("id") Long id) {
+    public ApiResponse<ResDto> getOne(@PathParam("id") UUID id) {
         Entity entity = service().findById(id);
-        return ApiResponse.success("Success get " + moduleName(), mapper().toResponse(entity));
+        return ApiResponse.success("Success get " + moduleName(), service().toDto(entity));
     }
 
     @POST
-    public ApiResponse<ResDto> create(ReqDto payload) {
+    public ApiResponse<ResDto> create(@Valid @ConvertGroup(to = OnCreate.class) ReqDto payload) {
         Entity entity = service().save(payload);
-        return ApiResponse.success("Success create " + moduleName(), mapper().toResponse(entity));
+        return ApiResponse.success("Success create " + moduleName(), service().toDto(entity));
     }
 
     @PATCH
     @Path("/{id}")
-    public ApiResponse<ResDto> update(@PathParam("id") Long id, ReqDto payload) {
+    public ApiResponse<ResDto> update(@PathParam("id") UUID id, @Valid @ConvertGroup(to = OnUpdate.class) ReqDto payload) {
         Entity entity = service().update(id, payload);
-        return ApiResponse.success("Success update " + moduleName(), mapper().toResponse(entity));
+        return ApiResponse.success("Success update " + moduleName(), service().toDto(entity));
     }
 
     @DELETE
     @Path("/{id}")
-    public ApiResponse<String> delete(@PathParam("id") Long id) {
+    public ApiResponse<String> delete(@PathParam("id") UUID id) {
         service().remove(id);
         return ApiResponse.success("Success delete " + moduleName() + " with ID: " + id);
     }
--- src/main/java/najib/io/common/BaseService.java
+++ src/main/java/najib/io/common/BaseService.java
@@ -1,15 +1,13 @@
 package najib.io.common;
 
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
-import jakarta.validation.groups.ConvertGroup;
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.NotFoundException;
-import najib.io.utils.validation.OnCreate;
-import najib.io.utils.validation.OnUpdate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public abstract class BaseService<Entity extends BaseEntity, ReqDto, ResDto extends BaseResDto> {
     protected abstract BaseRepository<Entity> repository();
@@ -18,11 +16,37 @@ public abstract class BaseService<Entity extends BaseEntity, ReqDto, ResDto exte
 
     protected abstract String moduleName();
 
+    protected abstract ResDto toDto();
+
+    protected abstract void mapDto(Entity entity, ResDto resDto);
+
+    protected ResDto toDto(Entity entity) {
+        ResDto resDto = toDto();
+
+        mapDto(entity, resDto);
+
+        resDto.setId(entity.getId());
+        resDto.setCreatedAt(entity.getCreatedAt());
+        resDto.setUpdatedAt(entity.getUpdatedAt());
+
+        return resDto;
+    }
+
+    protected List<ResDto> toDto(List<Entity> entities) {
+        List<ResDto> dtos = new ArrayList<>();
+
+        for (Entity entity : entities) {
+            dtos.add(toDto(entity));
+        }
+
+        return dtos;
+    }
+
     protected List<Entity> findAll(int page, int size, String sortField, String sortOrder, Map<String, String> filters) {
         return repository().findPaginated(page, size, sortField, sortOrder, filters);
     }
 
-    protected Entity findById(Long id) {
+    protected Entity findById(UUID id) {
         Entity entity = repository().findById(id);
         if (entity == null) {
             throw new NotFoundException(moduleName() + " not found");
@@ -31,20 +55,20 @@ public abstract class BaseService<Entity extends BaseEntity, ReqDto, ResDto exte
     }
 
     @Transactional
-    protected Entity save(@Valid @ConvertGroup(to = OnCreate.class) ReqDto dto) {
+    protected Entity save(ReqDto dto) {
         Entity entity = mapper().toEntity(dto);
         repository().persist(entity);
         return entity;
     }
 
     @Transactional
-    protected Entity update(Long id, @Valid @ConvertGroup(to = OnUpdate.class) ReqDto dto) {
+    protected Entity update(UUID id, ReqDto dto) {
         Entity entity = findById(id);
         return mapper().toEntity(dto, entity);
     }
 
     @Transactional
-    protected void remove(Long id) {
+    protected void remove(UUID id) {
         findById(id);
         boolean isDeleted = repository().softDelete(id);
         if (!isDeleted) {
